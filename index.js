5010e470427bfdd0a47ce9c80b6789ca6f834faa
Vlad <vladtsibizov@gmail.com>
2022-04-24 06:02:02 +0100

    hi

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const Gpio = require('pigpio').Gpio;
 
-io.on('connection', () => {
-  console.log('connected')
+
+const pins = {}
+
+for (let i = 2; i < 28; i++) {
+  pins[i] = new Gpio(i.toString(), 'out')
+}
+
+Object.values(pins).forEach((pin) => {
+  pin.digitalWrite(0)
 })
+
+
+io.on('connection', (socket) => {
+  console.log('connected')
+
+  Object.entries(pins).forEach((pin) => {
+    if (pin[1].digitalRead()) {
+      socket.emit('on', pin[0])
+    }
+  })
+
+  socket.on('toggle', (pinNumber) => {
+    const pin = pins[pinNumber]
+    if (!pin.digitalRead()) {
+      pin.digitalWrite(1)
+      socket.emit('on', pinNumber)
+      socket.broadcast.emit('on', pinNumber)
+    } else {
+      pin.digitalWrite(0)
+      socket.emit('off', pinNumber)
+      socket.broadcast.emit('off', pinNumber)
+    }
+  })
+});
+
+process.on('SIGINT', () => {
+  Object.values(pins).forEach((pin) => {
+    pin.digitalWrite(0)
+  })
+    process.exit()
+});
 
 
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3000);
+server.listen(3000)
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,20 @@
-$(function(){
+$(function () {
+  const socket = io()
+
+  socket.on('on', (pin) => {
+    const el = $(`button[data-src=${pin}]`)[0]
+    el.classList.add('enabled')
+  })
+
+  socket.on('off', (pin) => {
+    const el = $(`button[data-src=${pin}]`)[0]
+    el.classList.remove('enabled')
+  })
+
   $('button').each((i, btn) => {
-    btn.addEventListener('click', function() {
-      console.log(this.getAttribute('data-src'))
+    btn.addEventListener('click', function () {
+      const pin = this.getAttribute('data-src')
+      socket.emit('toggle', pin)
     })
   })
 });
